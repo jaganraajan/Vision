b748f945ad3979df40397630a94d97fded82284d
Jadmsaad <jad.m.saad@gmail.com>
2020-02-01 12:12:07 -0500

    connect to database

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db')
+
+
 
 const app = express();
 
+
+connectDB();
 
 app.get('/', (req,res) => res.send('API Running'));
 
